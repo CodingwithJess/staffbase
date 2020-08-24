d80de8e2a2b2e08f750af090bd2aac1c602c4958
CodingwithJess <thejessiebess@gmail.com>
2020-08-23 20:26:26 -0600

    starting basic file structure andstarting component parts for main DataArea

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Employee Directory</h1>
+      <p>Click on arrows to rearrange alphabetically or use search to narrow results.</p>
+    </div>
+  )
+}
+
+export default Header;
---
+++ src/utils/API.js
@@ -0,0 +1,8 @@
+// import axios for "axios";
+
+// export default {
+//   // get all users function
+//   getUsers: function()=>{
+//     return axios.get("https:/randomuser.me/api/?results=200&nat=us")
+//   }
+// };
