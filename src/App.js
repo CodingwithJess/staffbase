c4478086f1520b03de9994d10b7a55755b6722de
CodingwithJess <thejessiebess@gmail.com>
2020-08-23 21:03:51 -0600

    updated file structure and added sort function to dataArea component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import Main from './components/Main/Main';
+import Wrapper from './components/Wrapper';
+import Header from './components/Header/Header';
+// import logo from './logo.svg';
 import './App.css';
-
+// main setup on app visually
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Wrapper>
+        <Header/>
+        <Main/>
+      </Wrapper>
     </div>
-  );
+    );
 }
 
 export default App;
--- src/components/DataArea.js
+++
@@ -1,6 +0,0 @@
-import React, {useState, useEffect} from "react";
-import DataTable from "./DataTable";
-import Navbar from "./Nav";
-import API from "../utils/API";
-import "../styles/DataArea.css";
-import DataAreaContext from "../utils/DataAreaContext";
