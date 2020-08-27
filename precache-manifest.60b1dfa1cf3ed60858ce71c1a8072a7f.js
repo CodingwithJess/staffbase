961b25fbd61af1d470367f7ab2d3aa8c23d3da47
CodingwithJess <thejessiebess@gmail.com>
2020-08-26 20:06:05 -0600

    Updates

---
+++ precache-manifest.60b1dfa1cf3ed60858ce71c1a8072a7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9625759e3802cfe8f7aeb175b74f960",
+    "url": "/staffbase/index.html/index.html"
+  },
+  {
+    "revision": "6993900eef5599caf7ca",
+    "url": "/staffbase/index.html/static/css/main.d32ad2e2.chunk.css"
+  },
+  {
+    "revision": "3c33ff5c6e1b4dd6c7c0",
+    "url": "/staffbase/index.html/static/js/2.1c998044.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/staffbase/index.html/static/js/2.1c998044.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6993900eef5599caf7ca",
+    "url": "/staffbase/index.html/static/js/main.67c703a8.chunk.js"
+  },
+  {
+    "revision": "7b55332d04b4e7e7ce2a",
+    "url": "/staffbase/index.html/static/js/runtime-main.f5f5f1d4.js"
+  }
+]);
